b45394bfe40d5d890845e13f24b4c1038958e1e0
Hritik Pawar <73632558+unlikelycreator@users.noreply.github.com>
2024-08-25 17:35:03 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: 'http://localhost:5173', 
+  origin: 'https://bookkeeper01.netlify.app', 
 }));
 app.use(express.json());
 
